b59173cade8cddab4eb070ab8df425312215bd2e
Yuliia <djudit@ukr.net>
2023-11-27 00:58:56 +0200

    task 1 is finished

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,9 +1,32 @@
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
-const body = document.querySelector('body');
+const bodyBG = document.querySelector('body');
 
-// btnStart.addEventListener('click', startClick);
-// btnStop.addEventListener('click', stopClick);
+btnStart.addEventListener('click', startClick);
+btnStop.addEventListener('click', stopClick);
+
+btnStop.setAttribute('disabled', '');
+
+function startClick(event) {
+  if (event.target.nodeName !== 'BUTTON') {
+    return;
+  }
+
+  timerId = setInterval(() => {
+    bodyBG.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.setAttribute('disabled', '');
+  btnStop.removeAttribute('disabled');
+}
+
+function stopClick(event) {
+  if (event.target.nodeName !== 'BUTTON') {
+    return;
+  }
+  clearInterval(timerId);
+  btnStop.setAttribute('disabled', '');
+  btnStart.removeAttribute('disabled');
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
