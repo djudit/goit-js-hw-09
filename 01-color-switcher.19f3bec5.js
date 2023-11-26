107fafbdcb7925df93874c35ae913d6482c7d689
djudit <djudit@ukr.net>
2023-11-26 16:24:37 +0000

    Deploying to gh-pages from @ djudit/goit-js-hw-09@9c354b1091df1b2d35f6709133a418acc4703fa5 🚀

---
+++ 01-color-switcher.19f3bec5.js
@@ -0,0 +1,2 @@
+document.querySelector("button[data-start]"),document.querySelector("button[data-stop]"),document.querySelector("body");
+//# sourceMappingURL=01-color-switcher.19f3bec5.js.map
---
+++ 01-color-switcher.c2227923.js
@@ -0,0 +1,2 @@
+document.querySelector("button[data-start]"),document.querySelector("button[data-stop]"),document.querySelector("body");
+//# sourceMappingURL=01-color-switcher.c2227923.js.map
