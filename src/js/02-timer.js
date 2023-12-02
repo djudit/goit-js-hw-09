26cf58b72ee8b6e4e6a1327c064cd6539f1f89af
Yuliia <djudit@ukr.net>
2023-12-02 18:07:43 +0200

    task 2 finished

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,14 +2,11 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
-const input = document.querySelector('#datetime-picker');
 const btnStart = document.querySelector('button[data-start]');
-const timer = {
-  timerDays: document.querySelector('span[data-days]'),
-  timerHours: document.querySelector('span[data-hours]'),
-  timerMinutes: document.querySelector('span[data-minutes]'),
-  timerSeconds: document.querySelector('span[data-seconds]'),
-};
+const timerDays = document.querySelector('span[data-days]');
+const timerHours = document.querySelector('span[data-hours]');
+const timerMinutes = document.querySelector('span[data-minutes]');
+const timerSeconds = document.querySelector('span[data-seconds]');
 
 const timerStyle = document.querySelector('.timer');
 // const divField = document.querySelectorAll('.field');
@@ -21,37 +18,50 @@ timerStyle.style.marginTop = '20px';
 // divField.style.flexDirection = 'column';
 // divField.style.alignItems = 'center';
 
-btnStart.setAttribute('disabled', '');
-btnStart.addEventListener('click', handleClick);
-
-let delta = 0;
-
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0] <= Date.now()) {
+    const today = new Date();
+    if (selectedDates[0] - today <= 0) {
       Notiflix.Notify.failure('Please choose a date in the future');
+      btnStart.setAttribute('disabled', '');
+      return;
+    } else {
+      btnStart.removeAttribute('disabled', '');
     }
-    convertMs(selectedDates[0].getTime() - Date.now());
-    btnStart.removeAttribute('disabled', '');
+
+    btnStart.addEventListener('click', handleClick);
+
+    function handleClick() {
+      const id = setInterval(() => {
+        timer(selectedDates[0], id);
+      }, 1000);
+    }
   },
 };
 
-const datePicker = flatpickr(input, options);
+flatpickr('#datetime-picker', options);
 
-let today = new Date().getTime();
-let future = Date.parse(datePicker.selectedDates.join(''));
-delta = future - today;
-console.log(delta);
+function timer(future, id) {
+  const today = new Date();
+  const delta = future - today;
+
+  if (delta <= 0) {
+    clearInterval(id);
+    return;
+  }
+
+  createTimer(convertMs(delta));
+}
 
 function createTimer({ days, hours, minutes, seconds }) {
-  timer.timerDays.textContent = days;
-  timer.timerHours.textContent = hours;
-  timer.timerMinutes.textContent = minutes;
-  timer.timerSeconds.textContent = seconds;
+  timerDays.textContent = days;
+  timerHours.textContent = hours;
+  timerMinutes.textContent = minutes;
+  timerSeconds.textContent = seconds;
 }
 
 function addLeadingZero(value) {
@@ -71,20 +81,99 @@ function convertMs(ms) {
     Math.floor((((ms % day) % hour) % minute) / second)
   );
 
-  createTimer({ days, hours, minutes, seconds });
   return { days, hours, minutes, seconds };
 }
 
-let timerId = null;
+// ---------------------------------------------------------------
+// import flatpickr from 'flatpickr';
+// import 'flatpickr/dist/flatpickr.min.css';
+// import Notiflix from 'notiflix';
 
-function handleClick(event) {
-  timerId = setInterval(() => {
-    if (delta < 1000) {
-      convertMs(delta - 1000);
-      today += 1000;
-    } else {
-      clearInterval(timerId);
-      // return;
-    }
-  }, 1000);
-}
+// const input = document.querySelector('#datetime-picker');
+// const btnStart = document.querySelector('button[data-start]');
+// const timer = {
+//   timerDays: document.querySelector('span[data-days]'),
+//   timerHours: document.querySelector('span[data-hours]'),
+//   timerMinutes: document.querySelector('span[data-minutes]'),
+//   timerSeconds: document.querySelector('span[data-seconds]'),
+// };
+
+// const timerStyle = document.querySelector('.timer');
+// // const divField = document.querySelectorAll('.field');
+
+// timerStyle.style.display = 'flex';
+// timerStyle.style.gap = '20px';
+// timerStyle.style.marginTop = '20px';
+// // divField.style.display = 'flex';
+// // divField.style.flexDirection = 'column';
+// // divField.style.alignItems = 'center';
+
+// btnStart.setAttribute('disabled', '');
+// btnStart.addEventListener('click', handleClick);
+
+// let delta = 0;
+
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     if (selectedDates[0] <= Date.now()) {
+//       Notiflix.Notify.failure('Please choose a date in the future');
+//     }
+//     delta = selectedDates[0];
+//     // convertMs(selectedDates[0].getTime() - Date.now());
+//     btnStart.removeAttribute('disabled', '');
+//   },
+// };
+
+// const datePicker = flatpickr(input, options);
+
+// // let today = new Date().getTime();
+// // let future = Date.parse(datePicker.selectedDates.join(''));
+// // delta = future - today;
+// // console.log(delta);
+
+// function createTimer({ days, hours, minutes, seconds }) {
+//   timer.timerDays.textContent = days;
+//   timer.timerHours.textContent = hours;
+//   timer.timerMinutes.textContent = minutes;
+//   timer.timerSeconds.textContent = seconds;
+// }
+
+// function addLeadingZero(value) {
+//   return String(value).padStart(2, '0');
+// }
+
+// function convertMs(ms) {
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   const days = addLeadingZero(Math.floor(ms / day));
+//   const hours = addLeadingZero(Math.floor((ms % day) / hour));
+//   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+//   const seconds = addLeadingZero(
+//     Math.floor((((ms % day) % hour) % minute) / second)
+//   );
+
+//   createTimer({ days, hours, minutes, seconds });
+//   return { days, hours, minutes, seconds };
+// }
+
+// let timerId = null;
+
+// function handleClick(event) {
+//   timerId = setInterval(() => {
+//     let time = delta - new Date();
+//     if (delta < 1000) {
+//       convertMs(time);
+//       // today += 1000;
+//     } else {
+//       clearInterval(timerId);
+//       // return;
+//     }
+//   }, 1000);
+// }
